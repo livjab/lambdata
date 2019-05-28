0d55e6cca3f65241c319b275ccdba5fb6198eaf2
Liv Johnson <livjab@gmail.com>
2019-05-28 16:06:35 -0500

    Updated function

--- lambdata_livjab/null_handling.py
+++ lambdata_livjab/null_handling.py
@@ -1,3 +1,15 @@
+# fills all nulls with the mean value of its column
+
 def null_handler(df):
-    for column in df:
-        column.fillna(column.mean(), inplace=True)
+    df = df.fillna(df.mean())
+    return df
+
+"""
+Example code
+"""
+
+#d = {"col": [6, 7, np.nan, 9], "col2": [34, np.nan, 68, 48], "col3": [30, 20, 20, np.nan]}
+
+#df = pd.DataFrame(data=d).astype(float)
+
+# null_handler(df)
