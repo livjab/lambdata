ae6d8b62c608f5a459eff3128c4a6785eb85aa06
Liv Johnson <livjab@gmail.com>
2019-05-30 15:37:43 -0500

    got unit test working

--- lambdata_livjab/lambdata_livjab_test.py
+++ lambdata_livjab/lambdata_livjab_test.py
@@ -1,11 +1,16 @@
 import unittest
 from null_handling import ReplaceNulls
-
+import pandas as pd
+import numpy as np
 
 class null_test(unittest.TestCase):
 
     def test_nulls(self):
-        self.assertEqual(df.isnull().sum(), 0)
+        d = {"col": [6, 7, np.nan, 9],
+             "col2": [34, np.nan, 68, 48],
+             "col3": [30, 20, 20, np.nan]}
+        null_df = pd.DataFrame(data=d).astype(float)
+        self.assertEqual(ReplaceNulls.null_handler(self, null_df).isnull().sum().sum(), 0)
 
 if __name__ == '__main__':
     unittest.main()
