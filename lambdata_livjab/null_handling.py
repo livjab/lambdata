cee0ea04f320466c216832ba2b6ac66164e84906
Liv Johnson <livjab@gmail.com>
2019-05-30 14:33:57 -0500

    Changed code to incorporate class

--- lambdata_livjab/null_handling.py
+++ lambdata_livjab/null_handling.py
@@ -1,15 +1,25 @@
-# fills all nulls with the mean value of its column
+"""
+ Fills all nulls in a dataframe with the mean value of its column
+ """
 
-def null_handler(df):
-    df = df.fillna(df.mean())
-    return df
+class ReplaceNulls:
+
+    def __init__(self, df=None):
+        self.df = df
+
+    def null_handler(self, df):
+        df = df.fillna(df.mean())
+        return df
 
 """
 Example code
+
+d = {"col": [6, 7, np.nan, 9],
+     "col2": [34, np.nan, 68, 48],
+     "col3": [30, 20, 20, np.nan]}
+
+null_df = pd.DataFrame(data=d).astype(float)
+
+replace_nulls = ReplaceNulls()
+repalce_nulls.null_handler(null_df)
 """
-
-#d = {"col": [6, 7, np.nan, 9], "col2": [34, np.nan, 68, 48], "col3": [30, 20, 20, np.nan]}
-
-#df = pd.DataFrame(data=d).astype(float)
-
-# null_handler(df)
